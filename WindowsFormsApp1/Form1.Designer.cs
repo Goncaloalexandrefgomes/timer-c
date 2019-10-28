53f04a0977dd4a6fdfbd35c70e3621e564bef488
Gonçalo Gomes 11º4 <Gonalo Gomes 114@WIN-GESFLHLR4EE>
2019-10-28 18:07:29 +0000

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.segundos = new System.Windows.Forms.Button();
+            this.minutos = new System.Windows.Forms.Button();
+            this.horas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(98, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(182, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "RESET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Font = new System.Drawing.Font("LCD2", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // segundos
+            // 
+            this.segundos.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.segundos.Location = new System.Drawing.Point(182, 12);
+            this.segundos.Name = "segundos";
+            this.segundos.Size = new System.Drawing.Size(84, 32);
+            this.segundos.TabIndex = 6;
+            this.segundos.Text = "Segundos";
+            this.segundos.UseVisualStyleBackColor = true;
+            this.segundos.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Acerta_Relogio_s);
+            // 
+            // minutos
+            // 
+            this.minutos.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutos.Location = new System.Drawing.Point(98, 12);
+            this.minutos.Name = "minutos";
+            this.minutos.Size = new System.Drawing.Size(78, 32);
+            this.minutos.TabIndex = 5;
+            this.minutos.Text = "Minutos";
+            this.minutos.UseVisualStyleBackColor = true;
+            this.minutos.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Acerta_Relogio_m);
+            // 
+            // horas
+            // 
+            this.horas.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.horas.Location = new System.Drawing.Point(12, 12);
+            this.horas.Name = "horas";
+            this.horas.Size = new System.Drawing.Size(80, 32);
+            this.horas.TabIndex = 4;
+            this.horas.Text = "Horas";
+            this.horas.UseVisualStyleBackColor = true;
+            this.horas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Acerta_Relogio_h);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(273, 126);
+            this.Controls.Add(this.segundos);
+            this.Controls.Add(this.minutos);
+            this.Controls.Add(this.horas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button segundos;
+        private System.Windows.Forms.Button minutos;
+        private System.Windows.Forms.Button horas;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int contador = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+            label1.Text = "00:00:00";
+        }
+
+
+        private void mostrar()
+        {
+            Console.Beep();
+            int dia = 24 * (int)Math.Pow(60, 2);
+            if (contador < 0) contador += dia;
+            if (contador >= dia) contador -= dia;
+            label1.Text = TimeSpan.FromSeconds(contador).ToString();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            contador++;
+            mostrar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+        }
+
+        private void Horas_Click(object sender, EventArgs e)
+        {
+            contador += 3600;
+            mostrar();
+        }
+
+        private void Minutos_Click(object sender, EventArgs e)
+        {
+            contador += 60;
+            mostrar();
+        }
+
+
+        private void Acerta_Relogio(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Acerta_Relogio_s(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) contador += 1;
+            if (e.Button == MouseButtons.Right) contador -= 1;
+            mostrar();
+        }
+
+        private void Acerta_Relogio_m(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) contador += 60;
+            if (e.Button == MouseButtons.Right) contador -= 60;
+            mostrar();
+
+        }
+
+        private void Acerta_Relogio_h(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) contador += 3600;
+            if (e.Button == MouseButtons.Right) contador -= 3600;
+            mostrar();
+
+        }
+    }
+}
